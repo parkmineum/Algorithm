ccb5e90824f3caf9afb1b4d4573515de0f5295fd
Parkmineum <165489156+parkmineum@users.noreply.github.com>
2024-09-09 09:48:37 +0900

    [Silver II] Title: 랜선 자르기, Time: 4 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
@@ -1,40 +1,28 @@
 #include<iostream>
 using namespace std;
 
-int K, N, ans;
 int line[10001];
-int  maxi = 0;
-long long mid, high, low;
-
-int main()
-{
+int main(){
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-
-	cin >> K >> N;
-	
-	for (int i = 0; i < K; i++)
-	{
+	int K, N;  cin >> K >> N;
+    
+    int maxl = 0;
+	for (int i = 0; i < K; i++){
 		cin >> line[i];
-		if (maxi < line[i]) maxi = line[i];
+		if (line[i]>maxl) maxl = line[i];
 	}
-	low = 1;
-	high = maxi;
-	ans = 0;
-
-	while (low <= high)
-	{
-		mid = (low + high) / 2;
+	long long low = 1, high, ans = 0;
+    high = maxl; 
+	while (low <= high){
+		long long mid = (low + high) / 2;
 		int cnt = 0;
-		for (int i = 0; i < K; i++)
-			cnt += line[i] / mid;
+		for (int i = 0; i < K; i++)  cnt += line[i] / mid;
 
 		if (cnt >= N) {
 			low = mid + 1;
-			if (ans < mid) ans = mid;
+			if (mid > ans) ans = mid;
 		}
-		else {// (cnt < N)
-			high = mid - 1;
-		}
+		else high = mid - 1;
 	}
 	
 	cout << ans;
